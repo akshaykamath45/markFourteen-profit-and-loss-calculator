80ff5dedfa4d814c617db5f2e9875003d4929eb6
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2022-12-04 03:43:03 +0530

    displaying output

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ var submitButton=document.querySelector('#submit-btn')
 var output=document.querySelector('#output');
 
 function submitHandler(){
-    var ip=initialPrice.value;
-    var qty=qunatityOfStocks.value;
-    var curr=currentPrice.value;
+    //Type casting to Number
+    var ip=Number(initialPrice.value);
+    var qty=Number(qunatityOfStocks.value);
+    var curr=Number(currentPrice.value);
     calculateProfitandLoss(ip,qty,curr);
 }
 function calculateProfitandLoss(initial,quantity,current){
@@ -16,15 +17,15 @@ function calculateProfitandLoss(initial,quantity,current){
         //loss logic
         var loss=(initial-current)*quantity;
         var lossPercentage=(loss/initial)*100;
-        console.log(`Hey Loss is ${loss} and the percent is ${lossPercentage}%`)
+        output.innerText=(`Hey Loss is ${loss} and the percent is ${lossPercentage}%`)
     }else if(current>initial){
         //Profit Logic
         var profit=(current-initial)*quantity;
         var profitPercentage=(profit/initial)*100;
-        console.log(`Hey profit is ${profit} and the percent is ${profitPercentage}%`)
+        output.innerText=(`Hey profit is ${profit} and the percent is ${profitPercentage}%`)
     }   
     else{
-        console.log("No Profit,No Less");
+        output.innerText=("No Profit,No Less");
     }
 }
 submitButton.addEventListener('click',submitHandler)
