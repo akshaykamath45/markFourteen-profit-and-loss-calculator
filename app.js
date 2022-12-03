ad23a1502909b16cce7520cd20ba3a71f882f773
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2022-12-04 03:14:35 +0530

    added document query selectors

---
+++ app.js
@@ -0,0 +1,18 @@
+var initialPrice = document.querySelector('#initial-price');
+var qunatityOfStocks=document.querySelector('#stocks-quantity');
+var currentPrice=document.querySelector('#current-price');
+
+var submitButton=document.querySelector('#submit-btn')
+var output=document.querySelector('#output');
+
+function calculateProfitandLoss(initial,quantity,current){
+    if(initial>current){
+        //loss logic
+    }else if(current>logic){
+        //Profit Logic
+    }
+    else{
+        //rest of logic
+    }
+
+}
